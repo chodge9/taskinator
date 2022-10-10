6807cec449b5da502decfb0305c308aeb46d4bdc
Chantal Hodge <chantal_h09@yahoo.com>
2022-10-10 18:00:43 -0500

    add ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,11 @@
-var cities = [];
-var citySearchInputEl = document.querySelector("#searched-city");
-var weatherContainerEl=document.querySelector("#current-weather-container");
-var cityFormEl=document.querySelector("#city-search-form");
-var cityInputEl=document.querySelector("#city");
-var forecastTitle = document.querySelector("#forecast");
-var forecastContainerEl = document.querySelector("#fiveday-container");
-var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+};
 
-var getCityWeather = function(city){
-    var apiKey = "6eb39225f19081d854391179d7843a68"
-    var apiURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}"
-}
+buttonEl.addEventListener("click", createTaskHandler);
